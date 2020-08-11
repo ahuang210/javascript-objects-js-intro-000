f9433a1eaca3682e5c55596b55398340d822df08
Ada Huang <ada10huang@gmail.com>
2020-08-11 22:19:26 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,4 +6,4 @@ var updatePlaylist = (playlist, artistName, songTitle) => {
   return Object.assign({}, playlist, { [artistName]: songTitle })
 }
 
-updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi")
+updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
