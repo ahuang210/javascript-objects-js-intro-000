42c62ebb448ad0a4cefb1d01f073b192ba766adf
Ada Huang <ada10huang@gmail.com>
2020-08-11 22:46:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var updatePlaylist = (playlist, artistName, songTitle) => {
   return playlist
 }
 
-updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi")
 
 var removeFromPlaylist = (playlist, artistName) => {
   delete playlist[artistName];
