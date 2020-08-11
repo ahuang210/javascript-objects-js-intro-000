f77fad9444c3a5887dbfa574c7e10a258926de81
Ada Huang <ada10huang@gmail.com>
2020-08-11 22:31:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,7 @@
 var playlist = {"Slowdive": "Alison", "My Bloody Valentine": "Sometimes"};
 
 var updatePlaylist = (playlist, artistName, songTitle) => {
-  playlist[artistName] = songTitle
-  return playlist
+  return playlist[artistName] = songTitle
 }
 
 updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
